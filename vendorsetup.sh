d2b5c54cd35dd9259622073461e2038741374059
Biraru <muhammadbilalalisyed@gmail.com>
2024-12-29 23:53:04 +0500

    Revert "Removed vendorsetup.sh"
    This reverts commit 50c7dc496e86717fbdc6d0d4c96774f9beba4cf2.

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_crownqltekdi-user
+add_lunch_combo twrp_crownqltekdi-userdebug
+add_lunch_combo twrp_crownqltekdi-eng
