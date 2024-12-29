50c7dc496e86717fbdc6d0d4c96774f9beba4cf2
Biraru <muhammadbilalalisyed@gmail.com>
2024-12-29 20:18:53 +0500

    Removed vendorsetup.sh
    Removed vendorsetup.sh as it had no purpose except adding add_lunch_combo which is obsolete

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo twrp_crownqltekdi-user
-add_lunch_combo twrp_crownqltekdi-userdebug
-add_lunch_combo twrp_crownqltekdi-eng
